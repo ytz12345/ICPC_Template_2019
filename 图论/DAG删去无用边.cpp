f504f677b28020a617b58cc20f2832ee7d6b8e83
ytz12345 <1217783313@qq.com>
2019-04-23 02:50:09 -0400

    added DAG删去无用边

---
+++ 图论/DAG删去无用边.cpp
@@ -0,0 +1,16 @@
+/*
+无用边定义:对于边(u,v)如果存在从u到v不经过该边的另一条路径，则称该边无用
+时间复杂度:O(n^3)
+*/
+bool f[N][N];//i是否可达j
+
+vector <int> e[N];
+
+int main() {
+	rep(i, 1, n)
+		for (int j : e[i]) {
+			for (k, 1, n)
+				if (i != k && j != k && f[i][k] && f[k][j])
+					no_use_edge;
+		}
+}
