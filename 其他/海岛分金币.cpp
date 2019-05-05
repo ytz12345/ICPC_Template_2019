be096b459669b9a04d0f3f1b6b28fa8abd593369
ytz12345 <1217783313@qq.com>
2019-05-05 07:18:23 -0400

    added some comments

--- 其他/海岛分金币.cpp
+++ 其他/海岛分金币.cpp
@@ -1,3 +1,4 @@
+/*本题模型:每个人想最大化自己收益，死掉视为负收益，多种情况要让靠前选择的人收益尽量大*/
 #include <bits/stdc++.h>
 
 #define v first
@@ -22,6 +23,7 @@ int main() {
         for (int j = 1; j < i; j ++)
             b[j] = pr(a[i - 1][j], j);
         sort (b + 1, b + i, [&](pr x, pr y){return x.v != y.v ? (x.v < y.v) : (x.id > y.id);});
+        //按照是否容易满足来排序，因为容易满足的人消耗掉的金币比较少，也就使得当前的人获利最大
         int s = m, nd = (i - 1) / 2;
         for (int j = 1; j < i && nd; j ++) {
             nd --;
