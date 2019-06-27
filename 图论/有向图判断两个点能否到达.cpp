564ec6e9660a4932c57011864289b1a5deb3fa8f
ytz12345 <1217783313@qq.com>
2019-06-27 14:01:20 +0800

    update 有向图判断两点是否可达

--- 图论/有向图判断两个点能否到达.cpp
+++ 图论/有向图判断两个点能否到达.cpp
@@ -1,4 +1,9 @@
-/*时间O(n*m/32) 空间O(n*n/blk) blk随便取*/
+/* 原题:n个点的有向图，k对点(u,v)满足u可达v
+ * p对点(u,v)满足u不可达v，问这个图是否存在
+ * 解法:按照p对可达点直接连边构图，然后考虑验证
+ * a[i][j]=0/1表示i是否可达j，我们对j分块，bitset加速
+ * 时间O(n*m/32) 空间O(n*n/blk) blk随便取
+ */
 const int N = 1e5 + 2;
 const int BLK = 5000;
 int n, k, p;
@@ -46,7 +51,7 @@ int main() {
 	for (int i = 1; i <= n; i ++) {
 		for (int j : f[i]) {
 			if (bel[i] == bel[j]) return puts("NO"), 0;
-			ck[bel[i]].push_back(bel[j]);
+			ck[bel[i]].push_back(bel[j]);//check
 		}
 		f[i].clear();
 	}
@@ -79,7 +84,8 @@ int main() {
 		}
 		for (int j = 1; j <= sum; j ++)
 			for (int v : ck[j]) 
-				if (BLK * (i - 1) < v && v <= BLK * i && a[j][v -  BLK * (i - 1) - 1] == 1) {
+				if (BLK * (i - 1) < v && v <= BLK * i && 
+					a[j][v -  BLK * (i - 1) - 1] == 1) {
 					puts("NO");
 					return 0;
 				}
